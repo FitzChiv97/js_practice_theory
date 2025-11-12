57c320b6aa5c0ba7f4b90d56d9308c944c43f71b
FitzChiv97 <porter.morgan.97bb@gmail.com>
2025-11-12 08:21:02 +0300

    [practice] Completed katas 341

--- practice/codewars.js
+++ practice/codewars.js
@@ -3276,33 +3276,66 @@ let dir = {
 
 
 //Range Extraction
-function solution(list){
-  let range = [];
+// function solution(list){
+//   let range = [];
 
-  outer: for(let i = 0; i < list.length; i++) {
-    console.log(`start: ${list[i]}`);
-    console.log(`list: ${list}`);
-    console.log(`i: ${i}`);
-    if (list[i+1] - list[i] === 1) {
-      console.log(list[i]);
-      continue outer;
-    } else if (i > 1) {
-      console.log(`range end: ${list[i]}`);
+//   outer: for(let i = 0; i < list.length; i++) {
+//     console.log(`start: ${list[i]}`);
+//     console.log(`list: ${list}`);
+//     console.log(`i: ${i}`);
+//     if (list[i+1] - list[i] === 1) {
+//       console.log(list[i]);
+//       continue outer;
+//     } else if (i > 1) {
+//       console.log(`range end: ${list[i]}`);
 
-      range.push(`${list[0]}-${list[i]}`);
-      console.log(range);
+//       range.push(`${list[0]}-${list[i]}`);
+//       console.log(range);
 
-      console.log('splicing');
-      console.log(list.splice(0, i+1));
+//       console.log('splicing');
+//       console.log(list.splice(0, i+1));
 
-      i = -1;
-    } else {
-      range.push(list.splice(0, i+1).join(','));
-      console.log(range);
-      i = -1;
-    }; 
-  };
+//       i = -1;
+//     } else {
+//       range.push(list.splice(0, i+1).join(','));
+//       console.log(range);
+//       i = -1;
+//     }; 
+//   };
 
-  return range.join(',');
-};
-console.log(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20, 24, 27]));
+//   return range.join(',');
+// };
+// console.log(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20, 24, 27]));
+
+
+// Valid Braces
+function validBraces(braces){
+  let validBraces = ['()','[]','{}'];
+
+  function removeBraces(braces, validBracesId) {
+    braces = braces.split('');
+    braces.splice(validBracesId, 2);
+    return braces.join('');
+  }
+
+  while (braces.length > 0) {
+    let validBracesId = -1;
+
+    console.log(braces);
+
+    for(let el of validBraces) {
+      if (braces.indexOf(el) > -1) {
+        validBracesId = braces.indexOf(el);
+        braces = removeBraces(braces, validBracesId);
+      };
+    }
+
+    console.log(validBracesId);
+    console.log(braces);
+
+    if (validBracesId < 0) return false;
+  }
+
+  return true;
+}
+console.log(validBraces('([{}])'));
