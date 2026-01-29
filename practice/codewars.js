d2c0b58ac2cacc172125415c65562aee96edc566
FitzChiv97 <porter.morgan.97bb@gmail.com>
2026-01-29 10:08:56 +0300

    [practice] Completed katas: 356 4kuy 91.6%

--- practice/codewars.js
+++ practice/codewars.js
@@ -3426,3 +3426,67 @@ function nextBigger(n) {
 //    .filter((el,id,arr) => el !== arr[id+1]);
 // }
 // console.log(mergeArrays([1,2,5,4,12], [5,3,6,7,8,10]));
+
+
+// Add Length
+// const addLength = (str) => str.split(' ').map(el => el + ` ${el.length}`);
+// console.log(addLength('you will win'));
+
+
+// Reversing Words in a String
+// const reverse = str => str.split(' ').reverse().join(' ');
+// console.log(reverse('you will win'));
+
+
+// Plural
+// const plural = n => n !== 1;
+
+
+// Capitalization and Mutability
+// const capitalizeWord = word => word[0].toUpperCase() + word.slice(1);
+
+
+//Bin to Decimal
+// const binToDec = bin => parseInt(bin, 2);
+
+
+//Triple Trouble (2 solutions)
+// function tripleTrouble(one, two, three){ 
+//   let output = '';
+
+//   for(let i = 0; i < one.length; i++) {
+//     output += one[i] + two[i] + three[i];
+//   }
+
+//   return output;
+// }
+
+// const tripleTrouble = (one, two, three) => one.split('').map((char, id) => char + two[id] + three[id]).join('');
+
+
+//Remove duplicates from list
+// const distinct = arr => arr.filter((el, id, arr) => id === arr.indexOf(el));
+// console.log(distinct([1, 2, 1, 1, 3, 2]));
+
+
+//Multiplication table for number (2 solutions)
+// function multiTable(num) {
+//   let table = '';
+
+//   for(let i = 1; i <= 10; i++) {
+//     table += `${i} * ${num} = ${i * num}\n`;
+//   }
+
+//   return table.trim();
+// }
+
+// function multiTable(num) {
+//   return [...Array(10)].map((_, id) => `${id+1} * ${num} = ${(id+1) * num}`).join('\n');
+// }
+// console.log(multiTable(5));
+
+
+//Hello, Name or World!
+// const hello = name => 
+//   `Hello, ${name? name[0].toUpperCase() + name.slice(1).toLowerCase() + '!':'World!'}`
+// console.log(hello('aliCE'));
