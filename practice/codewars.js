9be00b023a4085abd5ed90aed92ae63c22675d03
FitzChiv97 <porter.morgan.97bb@gmail.com>
2026-02-10 22:26:41 +0300

    [practice] Completed katas 370

--- practice/codewars.js
+++ practice/codewars.js
@@ -3695,3 +3695,19 @@ function nextBigger(n) {
   
 //   return count;
 // }
+
+
+// Number of trailing zeros of N!
+// function zeros(n) {
+//   let degree = 2;
+//   let divisor = 5;
+//   let trailingZeroes = 0;
+
+//   while (divisor <= n) {
+//     trailingZeroes += Math.floor(n / divisor);
+//     divisor = (Math.pow(5, degree++));
+//   }
+
+//   return trailingZeroes;
+// }
+// console.log((zeros(1000)));
