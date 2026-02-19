e888c77d171452f6376b66a1862e13cf5bc93ca5
FitzChiv97 <porter.morgan.97bb@gmail.com>
2026-02-19 10:10:56 +0300

    [practice] Completed katas: 373 3kuy 9.0%

--- practice/codewars.js
+++ practice/codewars.js
@@ -3751,8 +3751,8 @@ let dir = {
 
 // console.log(getPINs('560198'));
 
-
-//validateBattlefield (3 kuy)
+/*
+//---------------validateBattlefield (3 kuy)-----------------------//
 function validateBattlefield(field) {
   let results = [];
   
@@ -3768,42 +3768,51 @@ function validateBattlefield(field) {
         
         // count length of ships and their amount 
         field[i][j] = 0;
-        let length = 1;
-        let step = 1;
-        
-        // horizontal check
-        let horizontalCell = field[i][j + step];
-        while(horizontalCell !== 0) {
-          // checkDiagonalCells clockwise around further horizontalCells 
-          if(!checkDiagonalCells(field[i - 1], field[i + 1], j + step)) return false;
-          // assign 0 to current horizontalCell
-          field[i][j + step] = 0;
-          step++;                                // here's an Error!
-          length++;
-          // move to the next horizontalCell
-          horizontalCell = field[i][j + step];
-          console.log(`step: ${step}`)
-        }
-        
-        // vertical check
-        let verticalCell = field[i + step][j];
-        console.log(`VERTICAL CELL: ${verticalCell}`);
-        while(verticalCell !== 0) {
-          // checkDiagonalCells clockwise around further verticalCells 
-          if(!checkDiagonalCells(field[i + (step - 1)], field[i + (step + 1)], j)) return false;
-          // assign 0 to current verticalCell
-          field[i + step][j] = 0;
-          step++;
-          length++;
-          // move to the next verticalCell
-          verticalCell = field[i + step][j];
-        }
-        console.log(length);
-        results.push(length);
+        results.push(checkShipLength(field, i, j));
       }
     }
   }
-  return results;
+  return checkResults(results);
+}
+
+function checkShipLength(field, rowId, cellId) {
+  let length = 1;
+  let step = 1;
+
+  // horizontal check
+  if(field[rowId][cellId + 1]) {
+    // not using [cellId + step] here to prevent an Error with verticalCell below
+    let horizontalCell = field[rowId][cellId + 1];
+      while(horizontalCell !== 0) {
+        console.log(`horizontalCell row: ${rowId}; cell: ${cellId + step}`);
+        // checkDiagonalCells clockwise around further horizontalCells 
+        if(!checkDiagonalCells(field[rowId - 1], field[rowId + 1], cellId + step)) return false;
+        // assign 0 to current horizontalCell
+        field[rowId][cellId + step] = 0;
+        step++;                                
+        length++;
+        // move to the next horizontalCell
+        horizontalCell = field[rowId][cellId + step];
+      }
+  }
+
+  // vertical check
+  if(field[rowId + 1][cellId]){
+    let verticalCell = field[rowId + 1][cellId];
+    while(verticalCell !== 0) {
+      console.log(`verticalCell row: ${rowId + step}; cell: ${cellId}`);
+      // checkDiagonalCells clockwise around further verticalCells 
+      if(!checkDiagonalCells(field[rowId + (step - 1)], field[rowId + (step + 1)], cellId)) return false;
+      // assign 0 to current verticalCell
+      field[rowId + step][cellId] = 0;
+      step++;
+      length++;
+      // move to the next verticalCell
+      verticalCell = field[rowId + step][cellId];
+    }
+  }
+
+  return length;
 }
 
 function checkDiagonalCells(upperRow, lowerRow, currentCellId) {
@@ -3830,7 +3839,30 @@ function checkDiagonalCells(upperRow, lowerRow, currentCellId) {
   return true;
 }
 
-validateBattlefield(
+function checkResults(results){
+  console.log(results);
+
+  let fieldRules = {
+    '1': 4,
+    '2': 3,
+    '3': 2,
+    '4': 1,
+  };
+
+  for (let shipCount in fieldRules) {    
+    for(let i = 0; i < shipCount; i++) {
+      console.log(`shipCount: ${shipCount}, shipLength: ${fieldRules[shipCount]}`);
+
+      let index = results.indexOf(fieldRules[shipCount]);
+      if(index === -1) return false;
+      results.splice(index, 1);
+    }
+  }
+
+  return !results.length;
+}
+
+let test = validateBattlefield(
  [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
   [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
   [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
@@ -3841,3 +3873,7 @@ validateBattlefield(
   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ]);
+
+console.log(test);
+//---------------------validateBattlefield (3 kuy)-----------------------//                   
+*/
